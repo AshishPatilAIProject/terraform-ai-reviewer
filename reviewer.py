c639e73755fe3f5a271f5687f6613fe0f7d11796
Ashish Patil <contactashishpatil@gmail.com>
2026-06-04 15:20:31 +0530

    tf file as argument

--- reviewer.py
+++ reviewer.py
@@ -2,9 +2,19 @@ import json
 from openai import OpenAI
 from dotenv import load_dotenv
 from terraform_reviewer import review_terraform
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "terraform_file",
+    help="Terraform file to review"
+)
+
+args = parser.parse_args()
 
 
-with open("sample2.tf", "r") as f:
+with open(args.terraform_file, "r") as f:
     terraform_code = f.read()
 
 review = review_terraform(terraform_code)
